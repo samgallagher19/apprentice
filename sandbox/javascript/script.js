db882ffc108050409ef495ad6738c2375eb155d5
Sam Sbgallagher Brooke Gallagher <sbgallagher@Sams-Air.hsd1.fl.comcast.net>
2021-09-06 17:28:17 -0400

    Updates with Javascript and dabbling with PHP

---
+++ sandbox/javascript/script.js
@@ -0,0 +1,4 @@
+function changeContent() {
+  document.getElementById("change_content").innerHTML = "Content is changed!";
+  document.getElementById("change_content").style.backgroundColor = "lightgreen";
+}
